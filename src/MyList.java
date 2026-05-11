d44e362191cd81cf2f31760d389c356259ac3bba
Nguyen Duc Anh <ducanh @gmail.com>
2026-05-12 00:23:40 +0700

    Hoan thanh bai 1

--- src/MyList.java
+++ src/MyList.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class MyList<E> {
     private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
@@ -5,6 +6,22 @@ public class MyList<E> {
 
     public MyList() {
         elements = new Object[DEFAULT_CAPACITY];
+    }
+    private void ensureCapa() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+    public void add(E e) {
+        if (size == elements.length) {
+            ensureCapa();
+        }
+        elements[size++] = e;
+    }
+    public E get(int i) {
+        if (i>= size || i <0) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i );
+        }
+        return (E) elements[i];
     }
 }
 
