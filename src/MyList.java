97300c91faa27b7199ceb7096ceab15948d20c65
Nguyen Duc Anh <ducanh @gmail.com>
2026-05-11 23:57:39 +0700

    Hoan thanh bai 1

---
+++ src/MyList.java
@@ -0,0 +1,10 @@
+public class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elements[];
+
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+}
+
